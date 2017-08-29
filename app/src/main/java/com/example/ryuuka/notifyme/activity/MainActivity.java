3c4cf0a96fd657b0886288c66de594866f142858
ryuuka <ryuuka.max@gmail.com>
2017-08-29 13:37:18 +0800

    Project startup

---
+++ app/src/main/java/com/example/ryuuka/notifyme/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.ryuuka.notifyme.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.ryuuka.notifyme.R;
+
+/**
+ * Created by ryuuka on 29/08/2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
